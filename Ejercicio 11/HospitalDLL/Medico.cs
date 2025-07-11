47914817cea509339034b90269ed3428f57b28b4
Brian <briancostanzo96@gmail.com>
2025-07-11 16:35:07 -0300

    Edite Paciente e HistorialClinico

--- Ejercicio 11/HospitalDLL/Medico.cs
+++ Ejercicio 11/HospitalDLL/Medico.cs
@@ -5,7 +5,38 @@ using System.Text;
 
 namespace HospitalDLL
 {
-    public class Medico : Persona
+    public class Medico
     {
-    }
+		private int _id;
+
+		public int ID
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private string _nombre;
+
+		public string nombre
+		{
+			get { return _nombre; }
+			set { _nombre = value; }
+		}
+
+		private string _apellido;
+
+		public string apellido
+		{
+			get { return _apellido; }
+			set { _apellido = value; }
+		}
+
+		private List<Especialidad> _especialidades;
+
+		public List<Especialidad> especialidades
+		{
+			get { return _especialidades; }
+			set { _especialidades = value; }
+		}
+	}
 }
